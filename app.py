347a5b0cf642b2f3cd39f8f7b906b52a9cc2b7b4
jcsargis00 <79231870+jcsargis00@users.noreply.github.com>
2022-06-02 23:56:30 -0700

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import datetime
 import dash
 from dash import dcc
 from dash.dependencies import Input, Output, State
-import dash_core_components as dcc
+# import dash_core_components as dcc
 import dash_html_components as html
 from dash import html
 import pandas as pd
