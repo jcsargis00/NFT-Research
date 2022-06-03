b8d1372ccf75392b1d58461b3f002fb40b829b24
jcsargis00 <79231870+jcsargis00@users.noreply.github.com>
2022-06-03 00:01:55 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 import datetime
 import dash
 from dash import dcc
-from dash.dependencies import Input, Output, State
-# import dash_core_components as dcc
+import dash_core_components as dcc
 import dash_html_components as html
+from dash.dependencies import Input, Output
 from dash import html
 import pandas as pd
 import yfinance as yf
